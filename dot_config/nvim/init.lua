e3d2d70e45cb7c9302ef4b5302f6876f51229486
Hyuga Tsukui <gakikiki407@gmail.com>
2023-07-03 22:10:35 +0900

    Add colorscheme.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -53,26 +53,4 @@ vim.call('jetpack#end')
 require('plugins.lsp-setup')
 require('plugins.git-client')
 require('plugins.fzf-setup')
-
--- colorscheme -----------------------------------------------------------------
-vim.cmd [[
-autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
-autocmd ColorScheme * highlight NonText ctermbg=none guibg=none
-autocmd ColorScheme * highlight LineNr ctermbg=none guibg=none
-autocmd ColorScheme * highlight Folded ctermbg=none guibg=none
-autocmd ColorScheme * highlight EndOfBuffer ctermbg=none guibg=none
-autocmd ColorScheme * highlight link FloatBorder NormalFloat
-autocmd ColorScheme * highlight Search guifg=#222240 guibg=#77A0E0
-
-colorscheme iceberg
-
-highlight LspReferenceText  ctermbg=8 guibg=#305090
-highlight LspReferenceRead  ctermbg=8 guibg=#305090
-highlight LspReferenceWrite ctermbg=8 guibg=#305090
-
-highlight link LspFloatWinNormal NormalFloat
-
-highlight FzfLuaNormal guibg=#383850
-highlight FzfLuaBorder guibg=#383850 gui=bold
-]]
-
+require('core.colorscheme')
