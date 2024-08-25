9ae0f3431d48a98e228b01588c437a4c24a12b59
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-08-25 18:26:35 +0900

    merge

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
 
 
+
+vim.keymap.set("n", "<leader>dq", ":lua vim.diagnostic.setloclist()<CR>")
