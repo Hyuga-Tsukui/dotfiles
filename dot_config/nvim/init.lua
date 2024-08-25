e7450f9deb2a7cebe09c8867c8bb86a092251878
Hyuga Tsukui <gakikiki407@gmail.com>
2024-08-25 13:09:21 +0900

    update

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,26 +1,26 @@
 if not vim.g.vscode then
-    require("core.keymaps")
-    vim.cmd[[colorscheme tokyonight]]
+    require("plugins-setup")
+	require("core.keymaps")
+	vim.cmd([[colorscheme tokyonight]])
 	local opt = vim.opt
 	opt.tabstop = 4
 	opt.expandtab = true
 	opt.shiftwidth = 4
 	opt.smartindent = true
 	opt.relativenumber = true
-    opt.cmdheight = 0
-    opt.conceallevel = 2
+	opt.cmdheight = 0
+	opt.conceallevel = 2
 
-    vim.keymap.set('t', '<C-w>h', "<C-\\><C-n><C-w>h",{silent = true})
+	vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { silent = true })
 
-    -- autocmd
-    vim.api.nvim_create_autocmd("TermOpen", {
-      pattern = "*",
-      callback = function()
-          opt.relativenumber = false
-          opt.number = false
-      end,
-    })
-
+	-- autocmd
+	vim.api.nvim_create_autocmd("TermOpen", {
+		pattern = "*",
+		callback = function()
+			opt.relativenumber = false
+			opt.number = false
+		end,
+	})
 
 	function Open_cheatsheet()
 		local che = vim.fn.stdpath("config") .. "/cheatsheet.md"
@@ -45,193 +45,13 @@ if not vim.g.vscode then
 	vim.opt.undodir = undodir
 	vim.opt.undofile = true
 
-    vim.diagnostic.config({
-        virtual_text = {
-            format = function(diagnostic)
-                return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-            end,
-        },
-    })
-
-	-- manage plugins.
-	vim.cmd.packadd("packer.nvim")
-	require("packer").startup(function()
-		use("wbthomason/packer.nvim")
-		-- use("rstacruz/vim-closer")
-		use("cohama/lexima.vim")
-		use("ibhagwan/fzf-lua")
-		use({
-			"neovim/nvim-lspconfig",
-			config = function()
-				local status_ok, lspconfig = pcall(require, "lspconfig")
-				if not status_ok then
-					print("lspconfig is not installed")
-					return
-				end
-
-				-- Start Language server.
-
-				-- Lua.
-				if lspconfig.lua_ls then
-					lspconfig.lua_ls.setup({
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = {
-										"vim",
-									},
-								},
-							},
-						},
-					})
-				end
-
-				-- TypeScript.
-				if lspconfig.tsserver then
-					lspconfig.tsserver.setup({})
-				end
-
-				-- OCaml.
-				if lspconfig.ocamllsp then
-					lspconfig.ocamllsp.setup({})
-				end
-
-				-- Go.
-				if lspconfig.gopls then
-					lspconfig.gopls.setup({})
-				end
-
-				if lspconfig.terraformls then
-					lspconfig.terraformls.setup({})
-				end
-
-				-- Lsp Keymaps.
-				-- global.
-				vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-				vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-				vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-
-				-- after the language server attaches to the current buffer
-				vim.api.nvim_create_autocmd("LspAttach", {
-					group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-					callback = function(ev)
-						vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-						local opts = { buffer = ev.buf }
-						vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-						vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-						vim.keymap.set("n", "<space>k", vim.lsp.buf.hover, opts)
-						vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-						vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					end,
-				})
+	vim.diagnostic.config({
+		virtual_text = {
+			format = function(diagnostic)
+				return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
 			end,
-		})
-		use({
-			"nvimtools/none-ls.nvim",
-			requires = { { "nvim-lua/plenary.nvim" } },
-			config = function()
-				local null_ls = require("null-ls")
-				null_ls.setup({
-					sources = {
-						null_ls.builtins.formatting.ocamlformat.with({
-							cwd = function(params)
-								conf = vim.fn.findfile(".ocamlformat", params.root)
-								if conf then
-									return vim.fn.fnamemodify(conf, ":p:h")
-								end
-							end,
-						}),
-						null_ls.builtins.formatting.stylua,
-						null_ls.builtins.formatting.gofmt,
-						null_ls.builtins.formatting.goimports,
-					},
-					on_attach = function(_, bufnr)
-						vim.keymap.set("n", "<space>f", function()
-							vim.lsp.buf.format({
-								async = true,
-								filter = function(client)
-									return client.name == "null-ls"
-								end,
-							})
-						end, { buffer = bufnr })
-					end,
-				})
-			end,
-		})
-
-		-- Lsp Complement.
-		use("hrsh7th/cmp-nvim-lsp")
-		use("hrsh7th/cmp-buffer")
-		use("hrsh7th/nvim-cmp")
-		use("L3MON4D3/LuaSnip")
-		use("saadparwaiz1/cmp_luasnip")
-
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			}),
-			{ name = "buffer" },
-		})
-        
-        -- Color
-        use("folke/tokyonight.nvim")
-        use({
-            "glidenote/memolist.vim",
-            config = function()
-                vim.g.memolist_path = vim.fn.expand("~/.config/memo/_posts")
-            end,
-        })
-
-        -- obsidian
-        use({
-          "epwalsh/obsidian.nvim",
-          tag = "*",  -- recommended, use latest release instead of latest commit
-          requires = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-
-            -- see below for full list of optional dependencies 👇
-          },
-          config = function()
-            require("obsidian").setup({
-              workspaces = {
-                {
-                  name = "personal",
-                  path = vim.fn.expand("~/obsidian")
-                },
-              },
-
-              -- see below for full list of options 👇
-              disable_frontmatter = true,
-              note_id_func = function(title)
-                -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-                -- In this case a note with the title 'My new note' will be given an ID that looks
-                -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-                local suffix = ""
-                if title ~= nil then
-                  -- If title is given, transform it into valid file name.
-                  suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-                else
-                  -- If title is nil, just add 4 random uppercase letters to the suffix.
-                  for _ = 1, 4 do
-                    suffix = suffix .. string.char(math.random(65, 90))
-                  end
-                end
-                return tostring(os.time()) .. "-" .. suffix
-              end,
-            })
-          end,
-        })
-	end)
+		},
+	})
 end
 
 local keymap = vim.keymap
