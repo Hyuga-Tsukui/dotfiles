c0626ca8dd57bddb4a73011165c7f87b3be038d1
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-08-27 10:36:34 +0900

    font size change

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 18
+config.font_size = 14
 config.color_scheme = 'tokyonight'
 
 local act = wezterm.action
