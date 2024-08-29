4ec2f32bcd7f430b2f59d209be8d014d38df49c2
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-08-29 18:03:50 +0900

    no-neckpain autoload

--- dot_config/nvim/lua/plugins/no-neck-pain.lua
+++ dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,7 +1,13 @@
 return {
 	"shortcuts/no-neck-pain.nvim",
-	lazy = true,
-	cmd = {
-		"NoNeckPain",
-	},
+	-- cmd = {
+	-- 	"NoNeckPain",
+	-- },
+    event = "VimEnter",
+	config = function()
+		-- vim.api.nvim_create_autocmd({"VimEnter"}, {
+		-- 	command = ":NoNeckPain<CR>",
+		-- })
+        vim.cmd("NoNeckPain")
+	end,
 }
