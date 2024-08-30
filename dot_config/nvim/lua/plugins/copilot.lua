e08fc64cc031aa3e2f29c66f2a24bd71dc785c75
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-08-30 21:27:03 +0900

    update

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -3,11 +3,10 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = { "Copilot" },
 		event = { "InsertEnter" },
+		build = ":Copilot auth",
 		config = function()
 			require("copilot").setup({})
+			-- require("copilot.suggestion").toggle_auto_trigger()
 		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
 	},
 }
