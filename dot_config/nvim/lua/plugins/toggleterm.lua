f6e27993b42daea583b9012203cb9391bec044e1
Hyuga Tsukui <gakikiki407@gmail.com>
2024-08-30 23:30:14 +0900

    update

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -5,7 +5,11 @@ return {
 	"akinsho/toggleterm.nvim",
 	keys = { trigger_key },
 	config = function()
-		require("toggleterm").setup({})
+		require("toggleterm").setup({
+            direction = "float",
+            size = 100,
+            close_on_exit = true,
+        })
 
 		vim.api.nvim_create_autocmd({ "TermEnter" }, {
 			pattern = { "term://*toggleterm#*" },
@@ -13,7 +17,6 @@ return {
 				vim.api.nvim_buf_set_keymap(0, "t", trigger_key, toggle, { noremap = true, silent = true })
 			end,
 		})
-        
 		vim.api.nvim_set_keymap("n", trigger_key, toggle, { noremap = true, silent = true })
 		vim.api.nvim_set_keymap("i", trigger_key, toggle, { noremap = true, silent = true })
 	end,
