9c805848a33370d1d4965c720ea7e314e88d5934
Hyuga Tsukui <gakikiki407@gmail.com>
2024-08-30 23:14:55 +0900

    refactor

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,50 +1,7 @@
 if not vim.g.vscode then
-	-- require("plugins-setup")
+	require("core.options")
 	require("core.keymaps")
 	require("config.lazy")
-	local opt = vim.opt
-	opt.tabstop = 4
-	opt.expandtab = true
-	opt.shiftwidth = 4
-	opt.smartindent = true
-	opt.relativenumber = true
-	opt.cmdheight = 0
-	opt.conceallevel = 2
-
-
-	vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h", { silent = true })
-
-	-- autocmd
-	vim.api.nvim_create_autocmd("TermOpen", {
-		pattern = "*",
-		callback = function()
-			opt.relativenumber = false
-			opt.number = false
-		end,
-	})
-
-	function Open_cheatsheet()
-		local che = vim.fn.stdpath("config") .. "/cheatsheet.md"
-		vim.cmd("split " .. che)
-		vim.cmd("setlocal readonly")
-		vim.cmd("resize 10")
-	end
-
-	-- keymaps.
-	vim.keymap.set("n", "[b", ":bprev<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "<C-j>", ":bprev<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "<C-k>", ":bnext<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
-	vim.keymap.set("n", "<leader>cs", "<cmd>lua Open_cheatsheet()<CR>", { noremap = true, silent = true })
-
-	vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>w", { noremap = true, silent = true })
-
-	local undodir = vim.fn.stdpath("config") .. "/undo"
-	if vim.fn.isdirectory(undodir) == 0 then
-		vim.fn.mkdir(undodir, "p")
-	end
-	vim.opt.undodir = undodir
-	vim.opt.undofile = true
 
 	vim.diagnostic.config({
 		virtual_text = {
@@ -56,8 +13,6 @@ if not vim.g.vscode then
 end
 
 local keymap = vim.keymap
-keymap.set("i", "jj", "<ESC>")
-keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 keymap.set("n", "<S-k>", "10<UP>")
 keymap.set("n", "<S-j>", "10<DOWN>")
@@ -68,5 +23,3 @@ keymap.set("v", "<S-k>", "10<UP>")
 keymap.set("v", "<S-j>", "10<DOWN>")
 keymap.set("v", "<S-h>", "10<LEFT>")
 keymap.set("v", "<S-l>", "10<RIGHT>")
-
-vim.opt.clipboard:append("unnamedplus")
