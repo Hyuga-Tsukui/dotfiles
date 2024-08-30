9cfad3015d12b6db5163cf20d66df1d8563dee04
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-08-30 21:27:03 +0900

    update

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,3 +1,31 @@
+local copilot = {
+	"zbirenbaum/copilot-cmp",
+	dependencies = "copilot.lua",
+	-- config = function(_, opts)
+	-- 	local copilot_cmp = require("copilot.cmp")
+	-- 	copilot_cmp.setup(opts)
+	--
+	-- 	LazyVim.lsp.on_attach(function(_)
+	-- 		copilot_cmp._on_insert_enter({})
+	-- 	end, "copilot")
+	-- end,
+	-- opts = function(_, opts)
+	-- 	table.insert(opts.sources, 1, {
+	-- 		name = "copilot",
+	-- 		group_index = 1,
+	-- 		priority = 100,
+	-- 	})
+	-- end,
+}
+
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	event = { "InsertEnter" },
@@ -8,6 +36,7 @@ return {
 		"hrsh7th/cmp-cmdline",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+		copilot,
 	},
 	config = function()
 		local cmp = require("cmp")
@@ -20,6 +49,7 @@ return {
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
+				{ name = "copilot", group_index = 2 },
 			}, {
 				{ name = "buffer" },
 			}),
@@ -29,6 +59,13 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = vim.schedule_wrap(function(fallback)
+					if cmp.visible() and has_words_before() then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						fallback()
+					end
+				end),
 			}),
 		})
 
