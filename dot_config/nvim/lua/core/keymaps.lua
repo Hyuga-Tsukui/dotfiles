b4a6198f4f7b6ab7534ae4dbd6120e9b78304923
Hyuga Tsukui <gakikiki407@gmail.com>
2024-08-30 23:14:55 +0900

    refactor

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -2,9 +2,8 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
-
-vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
-
-
-
 vim.keymap.set("n", "<leader>dq", ":lua vim.diagnostic.setloclist()<CR>")
+
+vim.keymap.set("n", "<C-j>", ":bprev<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -1,11 +1,15 @@
 local opt = vim.opt
 
 -- line numbers
-opt.number = true
+-- opt.number = true
+opt.relativenumber = true
+opt.cmdheight = 0
+opt.conceallevel = 2
 
+opt.smartindent = true
 -- tab & indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
@@ -43,3 +47,10 @@ opt.swapfile = false
 opt.updatetime = 500
 
 opt.iskeyword:append("-")
+
+local undodir = vim.fn.stdpath("config") .. "/undo"
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
+vim.opt.undodir = undodir
+vim.opt.undofile = true
