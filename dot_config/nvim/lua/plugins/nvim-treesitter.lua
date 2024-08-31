c5d37da441d2fadf4c88ca4abe4d330a48822463
Hyuga Tsukui <gakikiki407@gmail.com>
2024-08-31 17:42:12 +0900

    update

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,16 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			-- ensure_installed = 'maintained',
+            sync_install = false,
+
+            auto_install = true,
+			ensure_installed = {
+                "vimdoc",
+                "javascript",
+                "typescript",
+                "lua",
+                "go",
+            },
             highlight = {
                 enable = true,
             },
