516fb61ce5103b0414b38ca60d255fdd3aff1826
Hyuga Tsukui <gakikiki407@gmail.com>
2024-09-03 22:02:11 +0900

    nvimでmd表示時にtree-sitterの表示を制御する
    https://github.com/nvim-treesitter/nvim-treesitter/issues/2825#issuecomment-1547409415

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,19 +2,42 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-            sync_install = false,
+			sync_install = false,
 
-            auto_install = true,
+			ignore_install = {
+				"markdown",
+			},
+
+			auto_install = true,
 			ensure_installed = {
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "lua",
-                "go",
-            },
-            highlight = {
-                enable = true,
-            },
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"lua",
+				"go",
+			},
+			highlight = {
+				enable = true,
+			},
+		})
+        -- REF: https://github.com/nvim-treesitter/nvim-treesitter/issues/2825#issuecomment-1547409415
+		vim.api.nvim_create_autocmd({
+			"BufEnter",
+			"BufWinEnter",
+		}, {
+			pattern = { "*.md" },
+			callback = function()
+				vim.cmd("set conceallevel=0")
+			end,
+		})
+		vim.api.nvim_create_autocmd({
+			"BufLeave",
+			"BufWinLeave",
+		}, {
+			pattern = { "*.md" },
+			callback = function()
+				vim.cmd("set conceallevel=3")
+			end,
 		})
 	end,
 }
