46a1e04da103cbc36504d50875b9ee4066c65e1d
Hyuga Tsukui <gakikiki407@gmail.com>
2024-09-04 20:29:39 +0900

    Update .config/nvim/lua/core/options.lua Update .zshrc

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -1,63 +1,67 @@
 local opt = vim.opt
 
-opt.guicursor = ""
-
--- line numbers
-opt.nu = true
-opt.relativenumber = true
-opt.cmdheight = 0
-opt.conceallevel = 2
-
-opt.smartindent = true
--- tab & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
-
--- line wrapping
-opt.wrap = false
-
--- search settings
-opt.ignorecase = true
-opt.smartcase = true
-opt.incsearch = true
-opt.hlsearch = false
-
-opt.scrolloff = 8
-
--- cursorline
-opt.cursorline = true
-
 -- appearence
-opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
-opt.title = true
-
--- backspace
-opt.backspace = "indent,eol,start"
+opt.guicursor = ""
 
 -- clipboard
 opt.clipboard:append("unnamedplus")
+opt.scrolloff = 8
 
--- split windows
-opt.splitright = true
-opt.splitbelow = true
+-- search settings
+opt.incsearch = true
+opt.hlsearch = false
 
--- log
-opt.history = 500
-opt.updatetime = 50
+if not vim.g.vscode then
+	-- line numbers
+	opt.nu = true
+	opt.relativenumber = true
+	opt.cmdheight = 0
+	opt.conceallevel = 2
 
-opt.iskeyword:append("-")
+	opt.smartindent = true
+	-- tab & indentation
+	opt.tabstop = 4
+	opt.shiftwidth = 4
+	opt.expandtab = true
+	opt.autoindent = true
 
--- backup
-opt.swapfile = false
-opt.backup = false
+	-- line wrapping
+	opt.wrap = false
 
-local undodir = vim.fn.stdpath("config") .. "/undo"
-if vim.fn.isdirectory(undodir) == 0 then
-	vim.fn.mkdir(undodir, "p")
+	-- search settings
+	opt.ignorecase = true
+	opt.smartcase = true
+
+	-- cursorline
+	opt.cursorline = true
+
+	-- appearence
+	opt.termguicolors = true
+	opt.background = "dark"
+	opt.signcolumn = "yes"
+	opt.title = true
+
+	-- backspace
+	opt.backspace = "indent,eol,start"
+
+	-- split windows
+	opt.splitright = true
+	opt.splitbelow = true
+
+	-- log
+	opt.history = 500
+	opt.updatetime = 50
+
+	opt.iskeyword:append("-")
+
+	-- backup
+	opt.swapfile = false
+	opt.backup = false
+
+	local undodir = vim.fn.stdpath("config") .. "/undo"
+	if vim.fn.isdirectory(undodir) == 0 then
+		vim.fn.mkdir(undodir, "p")
+	end
+	vim.opt.undodir = undodir
+	vim.opt.undofile = true
 end
-vim.opt.undodir = undodir
-vim.opt.undofile = true
