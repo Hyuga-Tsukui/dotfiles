4e255bb7c1db11e7cea8cd0828f96f301f77a241
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-09-04 15:37:09 +0900

    vscode

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -1,15 +1,17 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
-vim.keymap.set("n", "<leader>dq", ":lua vim.diagnostic.setloclist()<CR>")
 
-vim.keymap.set("n", "<C-j>", ":bprev<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", ":bnext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
+-- vim.keymap.set("n", "<C-k>", ":bnext<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-j>", ":bprev<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", "\"_dp")
+vim.keymap.set("x", "<leader>p", '"_dp')
 
+if not vim.g.vscode then
+	vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+	vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
+	vim.keymap.set("n", "<leader>dq", ":lua vim.diagnostic.setloclist()<CR>")
+end
