535c10572d0c288e959470c2b23aa5c8e850ad05
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-09-04 15:37:09 +0900

    vscode

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,5 @@
 if not vim.g.vscode then
 	require("core.options")
-	require("core.keymaps")
 	require("config.lazy")
 
 	vim.diagnostic.config({
@@ -12,14 +11,5 @@ if not vim.g.vscode then
 	})
 end
 
-local keymap = vim.keymap
+require("core.keymaps")
 
--- keymap.set("n", "<S-k>", "10<UP>")
--- keymap.set("n", "<S-j>", "10<DOWN>")
--- keymap.set("n", "<S-h>", "10<LEFT>")
--- keymap.set("n", "<S-l>", "10<RIGHT>")
---
--- keymap.set("v", "<S-k>", "10<UP>")
--- keymap.set("v", "<S-j>", "10<DOWN>")
--- keymap.set("v", "<S-h>", "10<LEFT>")
--- keymap.set("v", "<S-l>", "10<RIGHT>")
