af098e05a2c8a3444b6229e99ca6c3521e1cef33
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-09-17 10:02:11 +0900

    Update .config/nvim/lua/core/keymaps.lua Update .config/nvim/lua/plugins/none-ls.lua

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -8,7 +8,13 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("x", "<leader>p", '"_dp')
+vim.keymap.set("n", "x", '"_x"', { noremap = true, silent = true })
+vim.keymap.set("n", "X", '"_D"', { noremap = true, silent = true })
+vim.keymap.set("x", "x", '"_x"', { noremap = true, silent = true })
+vim.keymap.set("o", "x", "d", { noremap = true, silent = true })
+
+vim.keymap.set("o", "i<space>", "iW", { noremap = true, silent = true })
+vim.keymap.set("x", "i<space>", "iW", { noremap = true, silent = true })
 
 if not vim.g.vscode then
 	-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
