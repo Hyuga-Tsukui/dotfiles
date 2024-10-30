44b29d67970c22ad7151d8665d7b4ed314ac6931
Hyuga Tsukui <gakikiki407@gmail.com>
2024-10-31 00:28:21 +0900

    Update .config/nvim/lua/plugins/barbar.lua

--- dot_config/nvim/lua/plugins/barbar.lua
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -6,6 +6,21 @@ return {
 	},
 	event = { "BufEnter" },
 	config = function()
+		require("barbar").setup({
+			sidebar_filetypes = {
+				-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+				NvimTree = true,
+				-- Or, specify the text used for the offset:
+				undotree = {
+					text = "undotree",
+					align = "center", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+				},
+				-- Or, specify the event which the sidebar executes when leaving:
+				["neo-tree"] = { event = "BufWipeout" },
+				-- Or, specify all three
+				Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
+			},
+		})
 		vim.keymap.set("n", "<TAB>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<S-TAB>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
 	end,
