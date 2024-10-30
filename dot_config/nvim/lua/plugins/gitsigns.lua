d28d0912f97037dcdab3dfae3ca4ec306fe254fd
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-10-30 17:04:53 +0900

    Update .config/nvim/lua/plugins/gitsigns.lua

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,29 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Actions
+				map("n", "<leader>hs", gitsigns.stage_hunk)
+				map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+				map("v", "<leader>hu", function()
+					gitsigns.undo_stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+			end,
+		})
 	end,
 }
