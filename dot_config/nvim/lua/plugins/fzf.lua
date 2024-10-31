d5953ba163011386c43b2f2330cb5711ba3290f6
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-10-31 17:26:19 +0900

    Update .config/nvim/lua/core/keymaps.lua Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,7 @@
 return {
 	"ibhagwan/fzf-lua",
 	config = function()
+		vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
 		vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
 	end,
 }
