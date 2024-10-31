fb4cd3ac96a728595a289cd2dbbd86d8c8533ba2
Hyuga Tsukui <gakikiki407@gmail.com>
2024-10-31 22:32:34 +0900

    Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,13 @@
 return {
 	"ibhagwan/fzf-lua",
 	config = function()
+		require("fzf-lua").setup({
+			keymap = {
+				fzf = {
+					["ctrl-q"] = "select-all+accept", -- send quick fix to quickfix list
+				},
+			},
+		})
 		vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
 		vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
 	end,
