6f8c9c83c5d756646df1aaa8889a7b7a78f88a44
Hyuga Tsukui <gakikiki407@gmail.com>
2024-10-31 22:49:21 +0900

    Update .config/nvim/lua/plugins/barbar.lua

--- dot_config/nvim/lua/plugins/barbar.lua
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -20,10 +20,23 @@ return {
 				-- Or, specify all three
 				Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
 			},
-            animation = false,
+			animation = false,
 		})
 		vim.keymap.set("n", "<TAB>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<S-TAB>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>p", "<Cmd>BufferPick<CR>", { noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>w", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>ac", "<Cmd>BufferCloseAllButCurrent<CR>", { noremap = true, silent = true })
+
+		local max_buf = 6
+		local function close_buffer()
+			local buffer_count = vim.fn.len(vim.fn.getbufinfo({ buflisted = 1 }))
+			if buffer_count >= max_buf then
+				vim.cmd("BufferCloseAllButCurrent")
+			end
+		end
+		vim.api.nvim_create_autocmd("BufEnter", {
+			callback = close_buffer,
+		})
 	end,
 }
