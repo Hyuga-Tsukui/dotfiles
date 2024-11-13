72fc5aab492d0675713308c932f1d4c57851208b
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2024-11-13 10:19:22 +0900

    Update .config/nvim/init.lua Update .config/nvim/lua/plugins/barbar.lua Update .config/nvim/lua/plugins/fidget.lua Update .config/nvim/lua/plugins/fzf.lua Update .config/nvim/lua/plugins/lexima.lua Update .config/nvim/lua/plugins/memolist.lua Update .config/nvim/lua/plugins/nvim-lspconfig.lua Update .config/nvim/lua/plugins/nvim-tree.lua Update .config/nvim/lua/plugins/nvim-treesitter.lua Update .config/nvim/lua/plugins/slimline.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,19 +1,18 @@
 if not vim.g.vscode then
-	require("core.options")
-	require("core.keymaps")
-	-- disable netrw at the very start of your init.lua
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
+    require("core.options")
+    require("core.keymaps")
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    vim.opt.laststatus = 0 -- status-line系のpluginへの切り替え時のちらつきを防ぐためにデフォルトは表示しないようにする.
 
-	vim.opt.laststatus = 3
+    require("config.lazy")
 
-	require("config.lazy")
-
-	vim.diagnostic.config({
-		virtual_text = {
-			format = function(diagnostic)
-				return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
-			end,
-		},
-	})
+    vim.diagnostic.config({
+        virtual_text = {
+            format = function(diagnostic)
+                return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+            end,
+        },
+    })
 end
