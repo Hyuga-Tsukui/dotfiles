7206dca07242733abbf6566c954487f670e4a76d
Hyuga Tsukui <gakikiki407@gmail.com>
2024-11-28 23:15:22 +0900

    Update .config/nvim/lua/plugins/none-ls.lua Update .config/nvim/lua/plugins/nvim-tree.lua

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -19,7 +19,7 @@ return {
         require("mason-null-ls").setup({
             handlers = {
                 prettierd = function() end, -- prettierd の自動セットアップを無効化
-                biome = function() end, -- biome の自動セットアップを無効化
+                biome = function() end,     -- biome の自動セットアップを無効化
             },
         })
 
@@ -51,7 +51,7 @@ return {
             -- sources の結合
             sources = vim.list_extend({
                 null_ls.builtins.formatting.terraform_fmt, -- デフォルト
-            }, javascript_project()),          -- JavaScript プロジェクト用のフォーマッター
+            }, javascript_project()),                      -- JavaScript プロジェクト用のフォーマッター
             on_attach = function(client, bufnr)
                 vim.keymap.set("n", "<space>f", function()
                     vim.lsp.buf.format({ async = true })
