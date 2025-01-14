e860cb7a84934eab409654ecf8394414b6007e21
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-01-14 12:35:27 +0900

    Add .config/nvim/lua/plugins/mason-null-ls.lua

---
+++ dot_config/nvim/lua/plugins/mason-null-ls.lua
@@ -0,0 +1,11 @@
+return {
+    "williamboman/mason-null-ls.nvim",
+    lazy = true,
+    config = function()
+        require("mason")
+        require("mason-null-ls").setup({
+            automatic_installation = false,
+            handlers = {},
+        })
+    end,
+}
