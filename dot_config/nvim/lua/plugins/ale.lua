86b0cd80095f2797ce9a13528380217de947e52e
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-01-16 00:55:06 +0900

    Update .config/nvim/lua/plugins/ale.lua

--- dot_config/nvim/lua/plugins/ale.lua
+++ dot_config/nvim/lua/plugins/ale.lua
@@ -5,12 +5,15 @@ return {
 		g.ale_linters = {
 			lua = {},
 			javascript = {},
+			terraform = { "tflint" },
 		}
 		g.ale_fixers = {
 			lua = { "stylua" },
 			javascript = { "biome", "prettier" },
 			typescript = { "biome", "prettier" },
+			terraform = { "terraform" },
 		}
+		g.ale_terraform_fmt_options = "--write=true"
 		g.ale_fix_on_save = 1
 	end,
 }
