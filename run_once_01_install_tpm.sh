986dff7f1e7058d0aad4875f8cd7441b07b6b038
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-16 00:07:50 +0900

    chore: add catppuccin tmux plugin

--- run_once_01_install_tpm.sh
+++ run_once_01_install_tpm.sh
@@ -4,3 +4,13 @@ if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
     echo "Installing tmux plugin manager..."
     git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm" && "$HOME/.config/tmux/plugins/tpm/bin/install_plugins"
 fi
+
+if [ ! -d "$HOME/.config/tmux/plugins/catppuccin" ]; then
+    echo "Installing tmux catppuccin..."
+    mkdir -p ~/.config/tmux/plugins/catppuccin
+    git clone -b v2.1.2 https://github.com/catppuccin/tmux.git $HOME/.config/tmux/plugins/catppuccin/tmux
+    
+    echo "Installing cyberdream theme..."
+    CYBERDREAM=$(curl https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/refs/heads/main/extras/tmux/cyberdream.tmuxtheme)
+    echo "$CYBERDREAM" > $HOME/.config/tmux/plugins/catppuccin/tmux/themes/catppuccin_cyberdream.tmuxtheme
+fi
