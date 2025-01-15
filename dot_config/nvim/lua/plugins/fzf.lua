0cf2ecfca1033243e98eb200d4819c0fb7891b81
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-15 23:56:30 +0900

    Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,19 +1,19 @@
 return {
-    "ibhagwan/fzf-lua",
-    keys = {
-        "<C-p>",
-        "<C-r>",
-    },
-    cmd = "FzfLua",
-    config = function()
-        require("fzf-lua").setup({
-            keymap = {
-                fzf = {
-                    ["ctrl-q"] = "select-all+accept", -- send quick fix to quickfix list
-                },
-            },
-        })
-        vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
-        vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
-    end,
+	"ibhagwan/fzf-lua",
+	keys = {
+		"<C-p>",
+		"<C-r>",
+	},
+	cmd = "FzfLua",
+	config = function()
+		require("fzf-lua").setup({
+			keymap = {
+				fzf = {
+					["ctrl-q"] = "select-all+accept", -- send quick fix to quickfix list
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-p>", ":FzfLua files<CR>", { noremap = true })
+		vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
+	end,
 }
