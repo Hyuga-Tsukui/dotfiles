0d1b07c1b6ad1c1e27b5522abbbbbab8bf5f1a8b
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-15 23:39:01 +0900

    Update .config/nvim/lua/plugins/color-scheme.lua

--- dot_config/nvim/lua/plugins/color-scheme.lua
+++ dot_config/nvim/lua/plugins/color-scheme.lua
@@ -7,5 +7,8 @@ return {
 			transparent = true,
 		})
 		vim.cmd([[colorscheme cyberdream]])
+
+		vim.api.nvim_set_hl(0, "IncSearch", { bg = "NONE", bold = true, reverse = true })
+		vim.api.nvim_set_hl(0, "Search", { bg = "NONE", bold = true, reverse = true })
 	end,
 }
