e018724882ffe74bcf210c3823294f9851f5c2ff
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-01-17 00:09:42 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/color-scheme.lua Update .config/nvim/lua/plugins/slimline.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -45,5 +45,6 @@ if not vim.g.vscode then
 		-- require("plugins.wezterm-move"),
 		require("plugins.alpha-nvim"),
 		require("plugins.nvim-hlslens"),
+		-- require("plugins.transparent-nvim"),
 	})
 end
