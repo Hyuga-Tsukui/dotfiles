296a710cc88e9691e62f6a5d6b69fe316c8ffa72
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-19 21:01:57 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/config/lazy.lua Update .config/tmux/plugins.conf

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -1,50 +1,51 @@
 if not vim.g.vscode then
-	-- Bootstrap lazy.nvim
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-		if vim.v.shell_error ~= 0 then
-			vim.api.nvim_echo({
-				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-				{ out, "WarningMsg" },
-				{ "\nPress any key to exit..." },
-			}, true, {})
-			vim.fn.getchar()
-			os.exit(1)
-		end
-	end
-	vim.opt.rtp:prepend(lazypath)
-	require("lazy").setup({
-		-- require("plugins.ale"),
-		require("plugins.Comment"),
-		require("plugins.barbar"),
-		require("plugins.color-scheme"),
-		require("plugins.copilot"),
-		require("plugins.dial"),
-		require("plugins.fidget"),
-		require("plugins.fzf"),
-		require("plugins.gitsigns"),
-		require("plugins.harpoon"),
-		require("plugins.hlchunk"),
-		require("plugins.lexima"),
-		require("plugins.mason-null-ls"),
-		require("plugins.mason-nvim"),
-		require("plugins.memolist"),
-		require("plugins.no-neck-pain"),
-		require("plugins.none-ls"),
-		require("plugins.nvim-cmp"),
-		require("plugins.nvim-lspconfig"),
-		require("plugins.nvim-surround"),
-		require("plugins.nvim-tree"),
-		require("plugins.nvim-treesitter"),
-		require("plugins.obsidian"),
-		require("plugins.slimline"),
-		require("plugins.tmux-navigator"),
-		require("plugins.vim-maketable"),
-		-- require("plugins.wezterm-move"),
-		require("plugins.alpha-nvim"),
-		require("plugins.nvim-hlslens"),
-		-- require("plugins.transparent-nvim"),
-	})
+    -- Bootstrap lazy.nvim
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        if vim.v.shell_error ~= 0 then
+            vim.api.nvim_echo({
+                { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+                { out,                            "WarningMsg" },
+                { "\nPress any key to exit..." },
+            }, true, {})
+            vim.fn.getchar()
+            os.exit(1)
+        end
+    end
+    vim.opt.rtp:prepend(lazypath)
+    require("lazy").setup({
+        -- require("plugins.ale"),
+        require("plugins.Comment"),
+        require("plugins.barbar"),
+        require("plugins.color-scheme"),
+        require("plugins.copilot"),
+        require("plugins.dial"),
+        require("plugins.fidget"),
+        require("plugins.fzf"),
+        require("plugins.gitsigns"),
+        require("plugins.harpoon"),
+        require("plugins.hlchunk"),
+        require("plugins.lexima"),
+        require("plugins.mason-null-ls"),
+        require("plugins.mason-nvim"),
+        require("plugins.memolist"),
+        require("plugins.no-neck-pain"),
+        require("plugins.none-ls"),
+        require("plugins.nvim-cmp"),
+        require("plugins.nvim-lspconfig"),
+        require("plugins.nvim-surround"),
+        require("plugins.nvim-tree"),
+        require("plugins.nvim-treesitter"),
+        require("plugins.obsidian"),
+        require("plugins.slimline"),
+        require("plugins.tmux-navigator"),
+        require("plugins.vim-maketable"),
+        -- require("plugins.wezterm-move"),
+        require("plugins.alpha-nvim"),
+        require("plugins.nvim-hlslens"),
+        -- require("plugins.transparent-nvim"),
+        require("plugins.auto-session"),
+    })
 end
