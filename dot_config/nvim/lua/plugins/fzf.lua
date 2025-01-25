7b65f47a86ac9242d2d0c247bfac146e9e285002
Hyuga Tsukui <gakikiki407@gmail.com>
2025-01-25 15:53:48 +0900

    Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/plugins/fzf.lua Update .config/nvim/lua/plugins/none-ls.lua Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,36 +1,36 @@
 return {
-	"ibhagwan/fzf-lua",
-	keys = {
-		"<C-p>",
-		"<C-r>",
-	},
-	cmd = "FzfLua",
-	config = function()
-		-- keep the default actions
-		local actions = require("fzf-lua").actions
-		require("fzf-lua").setup({
-			keymap = {
-				fzf = {
-					["ctrl-q"] = "select-all+accept", -- send quick fix to quickfix list
-				},
-			},
-			actions = {
-				files = {
-					["ctrl-b"] = function() -- switch to buffers
-						require("fzf-lua").buffers()
-					end,
-					["default"] = actions.file_edit,
-				},
-				buffers = {
-					["ctrl-f"] = function() -- switch to files
-						require("fzf-lua").files()
-					end,
-					["default"] = actions.file_edit,
-				},
-			},
-		})
-		vim.keymap.set("n", "<C-p>f", ":FzfLua files<CR>", { noremap = true })
-		vim.keymap.set("n", "<C-p>b", ":FzfLua buffers<CR>", { noremap = true })
-		vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
-	end,
+    "ibhagwan/fzf-lua",
+    keys = {
+        "<C-p>",
+        "<C-r>",
+    },
+    cmd = "FzfLua",
+    config = function()
+        -- keep the default actions
+        local actions = require("fzf-lua").actions
+        require("fzf-lua").setup({
+            keymap = {
+                fzf = {
+                    ["ctrl-q"] = "select-all+accept", -- send quick fix to quickfix list
+                },
+            },
+            actions = {
+                files = {
+                    ["ctrl-b"] = function() -- switch to buffers
+                        require("fzf-lua").buffers()
+                    end,
+                    ["default"] = actions.file_edit,
+                },
+                buffers = {
+                    ["ctrl-f"] = function() -- switch to files
+                        require("fzf-lua").files()
+                    end,
+                    ["default"] = actions.file_edit,
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { noremap = true })
+        vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>", { noremap = true })
+        vim.keymap.set("c", "<C-r>", "<C-c>:FzfLua command_history<CR>", { noremap = true, silent = true })
+    end,
 }
