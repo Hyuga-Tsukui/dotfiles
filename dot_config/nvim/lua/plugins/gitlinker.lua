aa9645bdf4a0919fb4e92ae328958a9ff93af7a2
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-02-18 13:31:37 +0900

    Add .config/nvim/lua/plugins/gitlinker.lua

---
+++ dot_config/nvim/lua/plugins/gitlinker.lua
@@ -0,0 +1,10 @@
+-- GitLinkコマンドで、Vモードで選択した行のGitHubリンクをクリップボードにコピーする
+-- GitLink!とすることで、ブラウザで開くことも可能
+-- REF: https://github.com/linrongbin16/gitlinker.nvim
+return {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    config = function()
+        require("gitlinker").setup()
+    end,
+}
