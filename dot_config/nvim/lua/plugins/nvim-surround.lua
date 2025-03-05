62820cb455bc840cee6a7e6743ebcea86d532d7a
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-03-05 17:54:06 +0900

    change: nvim-surround trigger key

--- dot_config/nvim/lua/plugins/nvim-surround.lua
+++ dot_config/nvim/lua/plugins/nvim-surround.lua
@@ -1,10 +1,10 @@
 return {
-	"kylechui/nvim-surround",
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	keys = { "ys" },
-	config = function()
-		require("nvim-surround").setup({
-			-- Configuration here, or leave empty to use defaults
-		})
-	end,
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    keys = { "ys", "ds", "cs" },
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end,
 }
