20f47038c3ab5c53d71f7fd000b32ff482c412c2
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-03-28 17:30:56 +0900

    Update .config/nvim/lua/plugins/lsp/servers/lua_ls.lua

--- dot_config/nvim/lua/plugins/lsp/servers/lua_ls.lua
+++ dot_config/nvim/lua/plugins/lsp/servers/lua_ls.lua
@@ -1,18 +1,55 @@
 return function()
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({
+
+    ---@param names string[]
+    ---@return string[]
+    local function get_plugin_paths(names)
+        local plugins = require("lazy.core.config").plugins
+        local paths = {}
+        for _, name in ipairs(names) do
+            if plugins[name] then
+                table.insert(paths, plugins[name].dir .. "/lua")
+            else
+                vim.notify("Invalid plugin name: " .. name, vim.log.levels.WARN)
+            end
+        end
+        return paths
+    end
+
+    ---@param plugins string[]
+    ---@return string[]
+    local function library(plugins)
+        local paths = get_plugin_paths(plugins)
+        table.insert(paths, vim.fn.stdpath("config") .. "/lua")
+        table.insert(paths, vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua")
+        table.insert(paths, vim.env.VIMRUNTIME .. "/lua")
+        table.insert(paths, "${3rd}/luv/library")
+        table.insert(paths, "${3rd}/busted/library")
+        table.insert(paths, "${3rd}/luassert/library")
+        return paths
+    end
+
+    local lua_ls_opts = {
         settings = {
             Lua = {
+                runtime = {
+                    version = "LuaJIT",
+                },
                 diagnostics = {
-                    globals = {
-                        "vim",
-                    },
+                    globals = { "vim" },
                 },
                 workspace = {
-                    maxPreload = 10000, -- プリロードするファイル数を増やす
-                    preloadFileSize = 5000, -- 最大ファイルサイズをKB単位で設定
+                    maxPreload = 10000,
+                    preloadFileSize = 5000,
+                    library = library({ "lazy.nvim" }),
+                    checkThirdParty = false,
+                },
+                telemetry = {
+                    enable = false,
                 },
             },
         },
-    })
+    }
+
+    lspconfig.lua_ls.setup(lua_ls_opts)
 end
