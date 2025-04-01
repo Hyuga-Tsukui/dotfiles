d595a3bf40abeb07a56dd7bcd61966e4c2fa9a5c
Hyuga Tsukui <hyuga.tsukui@tkk.tokyu.co.jp>
2025-04-01 13:38:23 +0900

    Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/copilot/chat.lua Add .config/nvim/lua/plugins/copilot/init.lua Add .config/nvim/lua/plugins/markdown-preview.lua Update .config/nvim/lua/plugins/slimline.lua Add .config/nvim/lua/plugins/treejs.lua

---
+++ dot_config/nvim/lua/plugins/copilot/init.lua
@@ -0,0 +1,18 @@
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+    config = function()
+        -- REF: integration nvim-cmp https://github.com/zbirenbaum/copilot-cmp
+        require("copilot").setup({
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+
+            -- enable when git commit editor is opened
+            filetypes = {
+                gitcommit = true,
+            },
+        })
+    end,
+}
--- dot_config/nvim/lua/plugins/slimline.lua
+++ dot_config/nvim/lua/plugins/slimline.lua
@@ -1,23 +1,23 @@
 return {
-	"sschleemilch/slimline.nvim",
-	event = { "BufRead", "BufNewFile" },
-	cond = function()
-		local exclude_filetypes = { "NvimTree" }
-		return not vim.tbl_contains(exclude_filetypes, vim.bo.filetype)
-	end,
-	dependencies = "lewis6991/gitsigns.nvim",
-	config = function()
-		vim.opt.laststatus = 3
-		require("slimline").setup({
-			components = {
-				left = {
-					"mode",
-					"recording",
-					"path",
-					"git",
-				},
-			},
-			style = "fg",
-		})
-	end,
+    "sschleemilch/slimline.nvim",
+    event = { "BufRead", "BufNewFile" },
+    cond = function()
+        local exclude_filetypes = { "NvimTree" }
+        return not vim.tbl_contains(exclude_filetypes, vim.bo.filetype)
+    end,
+    dependencies = "lewis6991/gitsigns.nvim",
+    config = function()
+        vim.opt.laststatus = 3
+        require("slimline").setup({
+            components = {
+                left = {
+                    "mode",
+                    "recording",
+                    "path",
+                    "git",
+                },
+            },
+            style = "fg",
+        })
+    end,
 }
