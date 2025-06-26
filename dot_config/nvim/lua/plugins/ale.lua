f97f08ca91e6c06c37a250b3e58e3e188d24f839
Hyuga Tsukui <gakikiki407@gmail.com>
2025-06-26 23:41:08 +0900

    Add .config/nvim/lua/plugins/ale.lua Add .config/nvim/lua/plugins/copilot.lua Add .config/nvim/lua/plugins/lsp/servers/eslint.lua Add .config/nvim/lua/plugins/make-tree.lua Add .config/nvim/lua/plugins/plamo-translate.lua Add .config/nvim/lua/plugins/transparent-nvim.lua Add .config/nvim/lua/plugins/wezterm-move.lua

---
+++ dot_config/nvim/lua/plugins/ale.lua
@@ -0,0 +1,19 @@
+return {
+    "dense-analysis/ale",
+    config = function()
+        local g = vim.g
+        g.ale_linters = {
+            lua = {},
+            javascript = {},
+            terraform = { "tflint" },
+        }
+        g.ale_fixers = {
+            lua = { "stylua" },
+            javascript = { "biome", "prettier" },
+            typescript = { "biome", "prettier" },
+            terraform = { "terraform" },
+        }
+        g.ale_terraform_fmt_options = "--write=true"
+        g.ale_fix_on_save = 1
+    end,
+}
