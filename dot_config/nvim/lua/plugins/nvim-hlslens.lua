112df298389d5f8ee44e5148c7b81969cfd43609
Hyuga Tsukui <gakikiki407@gmail.com>
2025-06-27 14:37:49 +0900

    Update .config/nvim/lua/plugins/nvim-hlslens.lua

--- dot_config/nvim/lua/plugins/nvim-hlslens.lua
+++ dot_config/nvim/lua/plugins/nvim-hlslens.lua
@@ -15,15 +15,18 @@ return {
         end, kopts)
 
         -- replace the current word with the cursor position
+        local function escape_magic(str)
+            return vim.fn.escape(str, [[\/.*$^~[]])
+        end
         vim.keymap.set("n", "#", function()
-            local current_word = vim.fn.expand("<cword>")
-            vim.api.nvim_feedkeys(":%s/" .. current_word .. "//g", "n", false)
+            local cword = escape_magic(vim.fn.expand("<cword>"))
+            vim.api.nvim_feedkeys(":%s/" .. cword .. "//g", "n", false)
             -- :%s/word/CURSOR/g
             local ll = vim.api.nvim_replace_termcodes("<Left><Left>", true, true, true)
             vim.api.nvim_feedkeys(ll, "n", false)
             vim.opt.hlsearch = true
             hlslens.start()
-        end, opts)
+        end, kopts)
 
         vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
         vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
