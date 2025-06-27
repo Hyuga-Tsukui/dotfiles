1d415530995cccf9f45ed3fdf1bde0754350074c
Hyuga Tsukui <gakikiki407@gmail.com>
2025-06-27 14:55:59 +0900

    Add .config/nvim/lua/plugins/snacks.lua

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        -- bigfile = { enabled = false },
+        -- dashboard = { enabled = false },
+        -- explorer = { enabled = false },
+        -- indent = { enabled = false },
+        -- input = { enabled = true },
+        picker = { enabled = true },
+        -- notifier = { enabled = true },
+        -- quickfile = { enabled = true },
+        -- scope = { enabled = true },
+        -- scroll = { enabled = true },
+        -- statuscolumn = { enabled = true },
+        -- words = { enabled = true },
+    },
+    keys = {
+        {
+            "<leader>ff",
+            function()
+                Snacks.picker.files()
+            end,
+            desc = "Find Files",
+        },
+    },
+}
