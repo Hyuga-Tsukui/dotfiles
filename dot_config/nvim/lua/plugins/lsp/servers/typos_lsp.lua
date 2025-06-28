92cc1134fc41f7dbdf26fa5cf5b1308fc6e5be1a
Hyuga Tsukui <gakikiki407@gmail.com>
2025-06-28 16:26:12 +0900

    Update: lsp configuration for Terraform and remove unused servers

--- dot_config/nvim/lua/plugins/lsp/servers/typos_lsp.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    local lspconfig = require("lspconfig")
-    lspconfig.typos_lsp.setup({
-        init_options = {
-            config = "~/.config/typos/.typos.toml",
-        },
-    })
-end
