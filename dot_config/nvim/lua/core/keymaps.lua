72cdf9f2fbc5697e40ad3b7cfa905dd66d6cd5ab
Hyuga-Tsukui <gakikiki407@gmail.com>
2025-07-11 20:32:22 +0900

    Change: builtin opt

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -8,17 +8,17 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>dq", ":lua vim.diagnostic.setloclist()<CR>")
 
 vim.schedule(function()
-    vim.keymap.set("n", ";", ":", { noremap = true })
-    vim.keymap.set("n", ":", ";", { noremap = true })
+	vim.keymap.set("n", ";", ":", { noremap = true })
+	vim.keymap.set("n", ":", ";", { noremap = true })
 end)
 
 -- Toggle relative line numbers
-vim.keymap.set("n", "<leader>n", function()
-    if vim.wo.relativenumber then
-        vim.wo.number = false
-        vim.wo.relativenumber = false
-    else
-        vim.wo.number = true
-        vim.wo.relativenumber = true
-    end
-end, { desc = "Toggle relative line numbers" })
+-- vim.keymap.set("n", "<leader>n", function()
+--     if vim.wo.relativenumber then
+--         vim.wo.number = false
+--         vim.wo.relativenumber = false
+--     else
+--         vim.wo.number = true
+--         vim.wo.relativenumber = true
+--     end
+-- end, { desc = "Toggle relative line numbers" })
