32a9cf09fb57fcec418330bae7f77a181a7dd549
Hyuga-Tsukui <gakikiki407@gmail.com>
2025-07-11 20:41:53 +0900

    Change: plugins settings
    Change: plugins settings

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,42 +1,38 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufRead", "BufNewFile" },
-	cond = function()
-		-- 現在のバッファのファイルタイプが特定のもの (例: NvimTree) でないことを確認
-		local exclude_filetypes = { "NvimTree" }
-		return not vim.tbl_contains(exclude_filetypes, vim.bo.filetype)
-	end,
+    "lewis6991/gitsigns.nvim",
+    event = { "BufRead", "BufNewFile" },
+    cond = function()
+        -- 現在のバッファのファイルタイプが特定のもの (例: NvimTree) でないことを確認
+        local exclude_filetypes = { "NvimTree" }
+        return not vim.tbl_contains(exclude_filetypes, vim.bo.filetype)
+    end,
 
-	config = function()
-		require("gitsigns").setup({
-			on_attach = function(bufnr)
-				local gitsigns = require("gitsigns")
+    config = function()
+        require("gitsigns").setup({
+            on_attach = function(bufnr)
+                local gitsigns = require("gitsigns")
 
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
 
-				-- Actions
-				map("n", "<leader>hs", gitsigns.stage_hunk)
-				map("n", "<leader>hu", gitsigns.undo_stage_hunk)
-				map("v", "<leader>hs", function()
-					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-				map("v", "<leader>hu", function()
-					gitsigns.undo_stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-				map("v", "<leader>hr", function()
-					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end)
-
-				vim.api.nvim_set_hl(0, "GitSignsAddNr", { fg = "#4CAF50", bg = "#e0e2ea" })
-				vim.api.nvim_set_hl(0, "GitSignsChangeNr", { fg = "#FF9800", bg = "#e0e2ea" })
-				vim.api.nvim_set_hl(0, "GitSignsDeleteNr", { fg = "#F44336", bg = "#e0e2ea" })
-			end,
-			numhl = true,
-			signcolumn = false,
-		})
-	end,
+                -- Actions
+                map("n", "<leader>hs", gitsigns.stage_hunk)
+                map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+                map("v", "<leader>hs", function()
+                    gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("v", "<leader>hu", function()
+                    gitsigns.undo_stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("v", "<leader>hr", function()
+                    gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+            end,
+            numhl = true,
+            signcolumn = true,
+        })
+    end,
 }
