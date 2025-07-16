04292077241da6ef2558ef676c8a5cd324e6ab94
Hyuga-Tsukui <gakikiki407@gmail.com>
2025-07-16 21:55:01 +0900

    Add: overseer.nvim: Add key mappings and fidget.nvim: Customize notification window

--- dot_config/nvim/lua/plugins/fidget.lua
+++ dot_config/nvim/lua/plugins/fidget.lua
@@ -3,7 +3,35 @@ return {
     opts = {
         notification = {
             override_vim_notify = true,
+            window = {
+                relative = 'editor',
+                align = 'top',
+                x_padding = 10000,
+                winblend = 20,
+                border = {
+                    { '╭', 'FloatBorderFidget' },
+                    { '─', 'FloatBorderFidget' },
+                    { '╮', 'FloatBorderFidget' },
+                    { '│', 'FloatBorderFidget' },
+                    { '╯', 'FloatBorderFidget' },
+                    { '─', 'FloatBorderFidget' },
+                    { '╰', 'FloatBorderFidget' },
+                    { '│', 'FloatBorderFidget' },
+                },
+            },
+        },
+        progress = {
+            display = {
+                done_ttl = 3,
+            },
         },
     },
+    config = function(_, opts)
+        vim.api.nvim_set_hl(0, 'FloatBorderFidget', {
+            fg = '#ffffff', -- 枠線の色
+            -- bg = '#1e1e2e', -- 通知ウィンドウの背景に合わせる
+        })
+        require('fidget').setup(opts)
+    end,
     lazy = false,
 }
--- dot_config/nvim/lua/plugins/overseer.lua
+++ dot_config/nvim/lua/plugins/overseer.lua
@@ -14,4 +14,11 @@ return {
     config = function()
         require('overseer').setup({})
     end,
+    keys = {
+        { '<leader>tl', '<cmd>OverseerRun<cr>', desc = 'Overseer Run' },
+        { '<leader>tt', '<cmd>OverseerToggle<cr>', desc = 'Overseer Toggle' },
+        { '<leader>tc', '<cmd>OverseerClose<cr>', desc = 'Overseer Close' },
+        { '<leader>tr', '<cmd>OverseerRestart<cr>', desc = 'Overseer Restart' },
+        { '<leader>td', '<cmd>OverseerDelete<cr>', desc = 'Overseer Delete' },
+    },
 }
