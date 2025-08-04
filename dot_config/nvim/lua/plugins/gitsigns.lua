237c0d845b7f280810799df20f6997e8ef5174eb
Hyuga-Tsukui <gakikiki407@gmail.com>
2025-08-04 22:47:04 +0900

    changes

--- dot_config/nvim/lua/plugins/gitsigns.lua
+++ dot_config/nvim/lua/plugins/gitsigns.lua
@@ -30,6 +30,22 @@ return {
                 map('v', '<leader>hr', function()
                     gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
                 end)
+                -- Navigation
+                map('n', ']c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ ']c', bang = true })
+                    else
+                        gitsigns.nav_hunk('next')
+                    end
+                end)
+
+                map('n', '[c', function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ '[c', bang = true })
+                    else
+                        gitsigns.nav_hunk('prev')
+                    end
+                end)
             end,
             numhl = true,
             signcolumn = true,
