118d1e525db6848c3f9b73d911713b1a77ac73cd
Hyuga-Tsukui <gakikiki407@gmail.com>
2025-09-10 21:48:09 +0900

    update

--- dot_config/nvim/lua/plugins/gitlinker.lua
+++ dot_config/nvim/lua/plugins/gitlinker.lua
@@ -5,4 +5,8 @@ return {
     'linrongbin16/gitlinker.nvim',
     cmd = 'GitLink',
     opts = {},
+    keys = {
+        { '<leader>gy', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Copy GitHub link' },
+        { '<leader>gY', '<cmd>GitLink!<cr>', mode = { 'n', 'v' }, desc = 'Open GitHub link in browser' },
+    },
 }
