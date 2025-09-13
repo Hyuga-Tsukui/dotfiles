e7e09628bacf1945ea00481e7313a193cd0962b9
Hyuga Tsukui <gakikiki407@gmail.com>
2025-09-13 16:46:39 +0900

    lsp update for biome

--- dot_config/nvim/after/lsp/biome.lua
+++ dot_config/nvim/after/lsp/biome.lua
@@ -1,17 +1,14 @@
 return {
-    cmd = { "npx", "biome", "lsp-proxy" },
-    on_new_config = function(new_config, new_root_dir)
-        -- 環境変数で biome.json を指定
-        new_config.cmd_env = {
-            BIOME_CONFIG_PATH = new_root_dir .. "/biome.json",
-        }
+    cmd = { 'npx', 'biome', 'lsp-proxy' },
+    root_dir = function(bufnr, on_dir)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        local root = vim.fs.root(fname, { 'biome.json', '.git' })
+        on_dir(root)
     end,
-    root_dir = function(fname)
-        local util = require("lspconfig.util")
-        -- biome.json を探すロジック
-        return util.root_pattern("biome.json")(fname) -- biome.json を基準にルートディレクトリを設定
-            or util.find_git_ancestor(fname)    -- .git を基準にルートディレクトリを設定
-            or util.path.dirname(fname)         -- デフォルトは現在のファイルのディレクトリ
+    on_new_config = function(new_config, new_root_dir)
+        new_config.cmd_env = {
+            BIOME_CONFIG_PATH = new_root_dir .. '/biome.json',
+        }
     end,
     settings = {},
 }
