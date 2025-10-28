04fc51c617fec2c2fe5e18ea4bbe6b58e90994c7
Hyuga Tsukui <gakikiki407@gmail.com>
2025-10-28 14:12:39 +0900

    update

---
+++ dot_config/nvim/after/lsp/pyright.lua
@@ -0,0 +1,14 @@
+return {
+    --@type vim.lsp.Config
+    settings = {
+        pyright = {
+            disableOrganizeImports = true,
+        },
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                autoImportCompletions = true,
+            },
+        },
+    },
+}
