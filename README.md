41779b2ab2ac33b875eb693e652f3a903f8b7efc
Hyuga Tsukui <gakikiki407@gmail.com>
2026-01-04 22:05:19 +0900

    CLIツールの管理のをNixへ移行

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-# install
+## 設定ガイド
 
-[chezmoi](https://www.chezmoi.io/)
+このdotfilesは以下のツールを使って管理している。
+
+- [chezmoi](https://www.chezmoi.io/)
+  - dotfilesの管理。
+- [nix](https://github.com/NixOS/nix)
+  - GUI以外のCommandLineツールなどの管理。
+- [homebrew](https://brew.sh/)
+  - GUIツールの管理。（chezmoiのrunタスクで初期化）
+
+## 初期化手順
+
+1. chezmoiのインストールとdotfilesの展開
 
 ```sh
+cd
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply Hyuga-Tsukui
 ```
 
-## supported
+2. nixのインストールとプロファイルの有効化
 
-MacOS
+```sh
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+```
+
+```sh
+nix profile install .#my-packages
+```
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Minimal package definition for aarch64-darwin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs = { self, nixpkgs, neovim-nightly-overlay, }: {
+    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+      name = "my-packages-list";
+      paths = [
+        nixpkgs.legacyPackages.aarch64-darwin.bat
+        nixpkgs.legacyPackages.aarch64-darwin.curl
+        nixpkgs.legacyPackages.aarch64-darwin.delta
+        nixpkgs.legacyPackages.aarch64-darwin.eza
+        nixpkgs.legacyPackages.aarch64-darwin.fd
+        nixpkgs.legacyPackages.aarch64-darwin.fzf
+        nixpkgs.legacyPackages.aarch64-darwin.git
+        nixpkgs.legacyPackages.aarch64-darwin.gitmux
+        nixpkgs.legacyPackages.aarch64-darwin.jq
+        nixpkgs.legacyPackages.aarch64-darwin.mise
+        nixpkgs.legacyPackages.aarch64-darwin.ripgrep
+        nixpkgs.legacyPackages.aarch64-darwin.tmux
+        nixpkgs.legacyPackages.aarch64-darwin.typos
+        neovim-nightly-overlay.packages.aarch64-darwin.neovim
+        nixpkgs.legacyPackages.aarch64-darwin.volta
+        nixpkgs.legacyPackages.aarch64-darwin.zoxide
+        nixpkgs.legacyPackages.aarch64-darwin.zsh-completions
+        nixpkgs.legacyPackages.aarch64-darwin.zsh-autosuggestions
+      ];
+    };
+  };
+}
