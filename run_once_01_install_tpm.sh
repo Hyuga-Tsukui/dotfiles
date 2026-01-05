140b747682bebddfa81b025788f8cc23c1a48d0f
Hyuga Tsukui <gakikiki407@gmail.com>
2026-01-05 09:50:19 +0900

    tmuxプラグインマネージャーのインストール手順を改善し、不要なテーマインストール部分を削除

--- run_once_01_install_tpm.sh
+++ run_once_01_install_tpm.sh
@@ -29,24 +29,11 @@ print_error() {
 # TPM (tmux plugin manager)
 if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
     print_section "Installing tmux plugin manager"
-    git clone https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm" \
-        && "$HOME/.config/tmux/plugins/tpm/bin/install_plugins"
+
+    git clone --depth 1 https://github.com/tmux-plugins/tpm "$HOME/.config/tmux/plugins/tpm"
+    "$HOME/.config/tmux/plugins/tpm/bin/install_plugins"
+
     print_success "tmux plugin manager installed"
 else
     print_success "tmux plugin manager already installed"
-fi
-
-# Catppuccin theme
-if [ ! -d "$HOME/.config/tmux/plugins/catppuccin" ]; then
-    print_section "Installing tmux catppuccin"
-    mkdir -p "$HOME/.config/tmux/plugins/catppuccin"
-    git clone -b v2.1.2 https://github.com/catppuccin/tmux.git "$HOME/.config/tmux/plugins/catppuccin/tmux"
-    print_success "catppuccin theme cloned"
-
-    print_section "Installing cyberdream theme"
-    CYBERDREAM=$(curl -fsSL https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/refs/heads/main/extras/tmux/cyberdream.conf)
-    echo "$CYBERDREAM" > "$HOME/.config/tmux/plugins/catppuccin/tmux/themes/catppuccin_cyberdream.tmuxtheme"
-    print_success "cyberdream theme installed"
-else
-    print_success "tmux catppuccin already installed"
 fi
