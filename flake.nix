89919dfcf9878d7dea327aa8e6fa92acf79a7fc7
Hyuga Tsukui <gakikiki407@gmail.com>
2026-01-11 05:15:15 +0900

    (refactor) flake.nix

--- flake.nix
+++ flake.nix
@@ -6,36 +6,41 @@
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, neovim-nightly-overlay, }: {
-    packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-      name = "my-packages-list";
-      paths = [
-        nixpkgs.legacyPackages.aarch64-darwin.adr-tools
-        nixpkgs.legacyPackages.aarch64-darwin.bat
-        nixpkgs.legacyPackages.aarch64-darwin.codex
-        nixpkgs.legacyPackages.aarch64-darwin.curl
-        nixpkgs.legacyPackages.aarch64-darwin.delta
-        nixpkgs.legacyPackages.aarch64-darwin.deno
-        nixpkgs.legacyPackages.aarch64-darwin.eza
-        nixpkgs.legacyPackages.aarch64-darwin.fd
-        nixpkgs.legacyPackages.aarch64-darwin.fzf
-        nixpkgs.legacyPackages.aarch64-darwin.gh
-        nixpkgs.legacyPackages.aarch64-darwin.ghq
-        nixpkgs.legacyPackages.aarch64-darwin.git
-        nixpkgs.legacyPackages.aarch64-darwin.gitmux
-        nixpkgs.legacyPackages.aarch64-darwin.jq
-        nixpkgs.legacyPackages.aarch64-darwin.mise
-        nixpkgs.legacyPackages.aarch64-darwin.uv
-        nixpkgs.legacyPackages.aarch64-darwin.ripgrep
-        nixpkgs.legacyPackages.aarch64-darwin.tmux
-        nixpkgs.legacyPackages.aarch64-darwin.tlrc
-        nixpkgs.legacyPackages.aarch64-darwin.typos
-        neovim-nightly-overlay.packages.aarch64-darwin.neovim
-        nixpkgs.legacyPackages.aarch64-darwin.volta
-        nixpkgs.legacyPackages.aarch64-darwin.zoxide
-        nixpkgs.legacyPackages.aarch64-darwin.zsh-completions
-        nixpkgs.legacyPackages.aarch64-darwin.zsh-autosuggestions
-      ];
+  outputs = { self, nixpkgs, neovim-nightly-overlay, }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.my-packages = pkgs.buildEnv {
+        name = "my-packages-list";
+        paths = [
+          pkgs.adr-tools
+          pkgs.bat
+          pkgs.codex
+          pkgs.curl
+          pkgs.delta
+          pkgs.deno
+          pkgs.eza
+          pkgs.fd
+          pkgs.fzf
+          pkgs.gh
+          pkgs.ghq
+          pkgs.git
+          pkgs.gitmux
+          pkgs.jq
+          pkgs.mise
+          pkgs.uv
+          pkgs.ripgrep
+          pkgs.tmux
+          pkgs.tlrc
+          pkgs.typos
+          neovim-nightly-overlay.packages.aarch64-darwin.neovim
+          pkgs.volta
+          pkgs.zoxide
+          pkgs.zsh-completions
+          pkgs.zsh-autosuggestions
+        ];
+      };
     };
-  };
 }
