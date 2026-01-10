256f9c1abddea140a302014f81ba84bbb62c0bd1
Hyuga Tsukui <gakikiki407@gmail.com>
2026-01-11 06:58:01 +0900

    install https://github.com/skanehira/ghost

--- flake.nix
+++ flake.nix
@@ -10,6 +10,23 @@
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      ghost = pkgs.stdenvNoCC.mkDerivation rec {
+        pname = "ghost";
+        version = "0.3.0";
+
+        src = pkgs.fetchzip {
+          url = "https://github.com/skanehira/ghost/releases/download/v${version}/ghost_aarch64-apple-darwin.tar.gz";
+          sha256 = "sha256-JJ2YcDKaXpynyUz8TaNj3q+DbRwhcl0cY3QBTAwUM4s=";
+        };
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          install -m 0755 ghost $out/bin/ghost
+          runHook postInstall
+        '';
+      };
     in
     {
       packages.${system}.my-packages = pkgs.buildEnv {
@@ -40,6 +57,7 @@
           pkgs.zoxide
           pkgs.zsh-completions
           pkgs.zsh-autosuggestions
+          ghost
         ];
       };
     };
