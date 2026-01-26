d548a7f5d7b5d697887a609075a7106c2b0ca4bc
Hyuga-Tsukui <gakikiki407@gmail.com>
2026-01-26 13:54:57 +0900

    update

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,13 +1,12 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.font = wezterm.font("UDEV Gothic 35NF")
+config.font = wezterm.font("UDEV Gothic 35NF", { weight = "Bold", italic = true })
 config.font_size = 17
-config.macos_window_background_blur = 100
-config.window_background_opacity = 1.0
+config.macos_window_background_blur = 20
+config.window_background_opacity = 0.9
 config.line_height = 1.1
 config.colors = require("cyberdream")
--- config.color_scheme = "rose-pine-moon"
 config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.window_decorations = "RESIZE"
 config.use_ime = true
